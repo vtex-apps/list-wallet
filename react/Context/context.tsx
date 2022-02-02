327bb762c75029ed5f43d99c8e97e596f2a2435e
Ana Maia <ana.baptistao@vtex.com.br>
2022-02-02 15:16:02 -0300

    functions

--- react/Context/context.tsx
+++ react/Context/context.tsx
@@ -1,16 +1,36 @@
 import React from 'react'
 
+import { ShowAlertOptions } from '../utils/showAlertOptions'
+
 interface ContextStoreType {
   button: boolean
   setButton: (button: boolean) => void
-  valueGiftCard: number
+  valueGiftCard: number | undefined
   code: string
+  addValueGiftCard: string | undefined
+  setAddValueGiftCard: (addValueGiftCard: string) => void
+  updateGiftCardFunction: () => void
+  validation: string
+  setValidation: (validation: string) => void
+  valueLists: number
+  showAlert: ShowAlertOptions
+  setShowAlert: (showAlert: ShowAlertOptions) => void
+  credit: number
 }
 const ContextStore = React.createContext<ContextStoreType>({
   button: false,
   setButton: () => {},
   valueGiftCard: 0,
   code: '',
+  addValueGiftCard: '',
+  setAddValueGiftCard: () => {},
+  updateGiftCardFunction: () => {},
+  validation: '',
+  setValidation: () => {},
+  valueLists: 0,
+  showAlert: ShowAlertOptions.notShow,
+  setShowAlert: () => {},
+  credit: 0,
 })
 
 export default ContextStore
--- react/Provider/provider.tsx
+++ react/Provider/provider.tsx
@@ -1,13 +1,111 @@
-import type { FC } from 'react'
-import React, { useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import { useMutation, useQuery } from 'react-apollo'
+import { useIntl } from 'react-intl'
 
 import ContextStore from '../Context/context'
+import updateGiftCard from '../queries/updateGiftCard.gql'
+import getValueTotalList from '../queries/getValueTotalList.gql'
+import getRouteRedemptionCode from '../queries/getRouteRedemptionCode.gql'
+import getValueGiftCard from '../queries/getValueGiftCard.gql'
+import getValueAlreadyInGiftCard from '../queries/getValueAlreadyInGiftCard.gql'
+import { ShowAlertOptions } from '../utils/showAlertOptions'
+import { provider } from '../utils/definedMessages'
 
 const ProviderStore: FC = (props) => {
+  const intl = useIntl()
   const [button, setButton] = useState(true)
+  const [addValueGiftCard, setAddValueGiftCard] = useState<string>()
+  const [validation, setValidation] = useState('')
+  const [valueGiftCard, setValueGiftCard] = useState(0)
+  const [valueLists, setValueLists] = useState(0)
+  const [code, setCode] = useState(intl.formatMessage(provider.withoutCode))
+  const [showAlert, setShowAlert] = useState(ShowAlertOptions.notShow)
+  const [credit, setCredit] = useState(0)
 
-  const valueGiftCard = 1350.55
-  const code = 'Codigo'
+  const [updateGiftCardMutation] = useMutation(updateGiftCard)
+  const { data: dataValueTotalList } = useQuery(getValueTotalList)
+  const { data: dataRedemptionCode } = useQuery(getRouteRedemptionCode)
+  const { data: dataGetValueGiftCard, refetch } = useQuery(getValueGiftCard)
+  const { data: dataGetValueAlreadyInGiftCard } = useQuery(
+    getValueAlreadyInGiftCard
+  )
+
+  useEffect(() => {
+    const giftCard = dataGetValueGiftCard?.getValueAlreadyInGiftCard
+    const list = dataValueTotalList?.getValueTotalList
+
+    if (giftCard && list) {
+      setCredit(list - giftCard)
+    }
+  }, [dataGetValueAlreadyInGiftCard, dataGetValueGiftCard, dataValueTotalList])
+
+  useEffect(() => {
+    const value = dataValueTotalList?.getValueTotalList
+
+    if (value) {
+      setValueLists(value)
+    }
+  }, [dataValueTotalList])
+
+  useEffect(() => {
+    const redemptionCode = dataRedemptionCode?.getRouteRedemptionCode
+
+    if (redemptionCode !== 'failed') {
+      setCode(redemptionCode)
+    }
+  }, [dataRedemptionCode])
+
+  useEffect(() => {
+    const value = dataGetValueGiftCard?.getValueGiftCard
+
+    if (value) {
+      setValueGiftCard(value)
+    }
+  }, [dataGetValueGiftCard])
+
+  function validationValue() {
+    if (addValueGiftCard === undefined) {
+      setValidation(intl.formatMessage(provider.missingValue))
+
+      return false
+    }
+
+    if (parseFloat(addValueGiftCard) <= 0) {
+      setValidation(intl.formatMessage(provider.negativeValue))
+
+      return false
+    }
+
+    if (parseFloat(addValueGiftCard) > credit) {
+      setValidation(
+        intl.formatMessage(provider.biggerThanCouldBe) + credit.toString()
+      )
+
+      return false
+    }
+
+    return true
+  }
+
+  async function updateGiftCardFunction() {
+    setShowAlert(ShowAlertOptions.notShow)
+    const valid = validationValue()
+
+    if (valid) {
+      const { data } = await updateGiftCardMutation({
+        variables: {
+          value: parseFloat(addValueGiftCard as string),
+        },
+      })
+
+      if (data) {
+        setShowAlert(ShowAlertOptions.alertSave)
+        refetch()
+      } else {
+        setShowAlert(ShowAlertOptions.alertError)
+      }
+    }
+  }
 
   return (
     <ContextStore.Provider
@@ -16,6 +114,15 @@ const ProviderStore: FC = (props) => {
         setButton,
         valueGiftCard,
         code,
+        addValueGiftCard,
+        setAddValueGiftCard,
+        updateGiftCardFunction,
+        validation,
+        setValidation,
+        valueLists,
+        showAlert,
+        setShowAlert,
+        credit,
       }}
     >
       {props.children}
