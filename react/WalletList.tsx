18f8d115d2ee2e0bd625ea41eb923329f20fe63b
Ana Maia <ana.baptistao@vtex.com.br>
2022-02-03 09:53:50 -0300

    chyange style

--- react/WalletList.tsx
+++ react/WalletList.tsx
@@ -1,28 +1,21 @@
 import React from 'react'
-import { useIntl } from 'react-intl'
 import { PageHeader } from 'vtex.styleguide'
 
 import './styles.global.css'
-import { wallet } from './utils/definedMessages'
 import ProviderStore from './provider/providerStore'
 import CollapsibleArea from './Components/collapsibleArea'
 
 const WalletList: StorefrontFunctionComponent = () => {
-  const intl = useIntl()
-
   return (
     <ProviderStore>
       <div className="bg-muted-5">
         <PageHeader
           title={
-            <>
-              <div className="display">
-                <div className="padding-pageheader">
-                  {intl.formatMessage(wallet.title)}
-                </div>
+            <div className="padding-pageheader">
+              <div>
+                <CollapsibleArea />
               </div>
-              <CollapsibleArea />
-            </>
+            </div>
           }
         />
       </div>
