6e18a5a09b2ed48ffa5e0282d7d14af5cefe9cb1
Ana Maia <ana.baptistao@vtex.com.br>
2022-02-08 13:24:12 -0300

    mocks

---
+++ react/__mocks__/values.tsx
@@ -0,0 +1,17 @@
+import { ShowAlertOptions } from '../utils/showAlertOptions'
+
+export const values = {
+  button: false,
+  setButton: jest.fn(),
+  valueGiftCard: 10,
+  code: 'code',
+  addValueGiftCard: '2',
+  setAddValueGiftCard: jest.fn(),
+  updateGiftCardFunction: jest.fn(),
+  validation: 'text validations',
+  setValidation: jest.fn(),
+  valueLists: 12,
+  showAlert: ShowAlertOptions.notShow,
+  handleCloseAlert: jest.fn(),
+  credit: 2,
+}
