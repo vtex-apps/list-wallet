99f4e08ae9445706c701d32ee19b79ea777bd2fa
Ana Maia <ana.baptistao@vtex.com.br>
2022-08-05 08:52:33 -0300

    css

--- react/Components/history.tsx
+++ react/Components/history.tsx
@@ -6,22 +6,21 @@ import { Spinner, Button, Modal } from 'vtex.styleguide'
 import { useStore } from '../hooks/useStore'
 
 const History: FC = () => {
-    const intl = useIntl()
+  const intl = useIntl()
 
-    const {
-        history,
-        loadingHistory
-    } = useStore()
+  const { history, loadingHistory, button } = useStore()
 
-    return (
-        <span className="ml3">
-            {loadingHistory ? (
-                <Spinner color="currentColor" size={20} />
-            ) : (
-                <Button variation="tertiary" size="small">Extrato2</Button>
-            )}
-        </span>
-    )
+  return (
+    <span className="ml3" style={{ position: 'absolute' }}>
+      {loadingHistory ? (
+        <Spinner color="currentColor" size={20} />
+      ) : (
+        <Button variation="tertiary" size="small">
+          Extrato
+        </Button>
+      )}
+    </span>
+  )
 }
 
 export default History
